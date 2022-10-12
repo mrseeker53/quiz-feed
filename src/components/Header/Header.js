0845d55046da8c7a5658922b3ce61b9271bb1087
mrseeker53 <mi.nayeem30@gmail.com>
2022-10-12 18:56:48 +0600

    added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import header from '../../Header.png'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='ml-12 md:flex justify-around items-center'>
+            <img className='w-auto md:w-2/3' src={header} alt="" />
+            <div>
+                <p className='text-5xl font-semibold text-blue-400 w-2/3 mx-3 mb-8'>Technical Quizzes to strong your technical knowledge</p>
+                <button className='bg-blue-600 text-white rounded-md px-4 py-2 ml-12'>Start Quiz</button>
+            </div>
         </div>
     );
 };
