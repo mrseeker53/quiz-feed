e639520d2e5d99b1a61d156e11ba934a4a7a51e8
mrseeker53 <mi.nayeem30@gmail.com>
2022-10-12 08:57:09 +0600

    added responsive menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navbar></Navbar>
+    }
+  ]);
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+    // Declare state
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            
-        </div>
+        <nav className='bg-blue-100 w-full h-20 flex items-center justify-between'>
+            {/* Brand Name */}
+            <p className='ml-12 md:w-80 md:ml-20 font-extrabold text-3xl text-blue-500 hover:text-teal-500'>
+                <Link to="/blog">Quiz Feed</Link>
+            </p>
+            {/* Add event handler to open/close hamburger menu */}
+            <div onClick={() => setOpen(!open)} className="h-6 w-6 mr-12 md:hidden" >
+                {
+                    // ternary operator where close : open
+                    open ? <XMarkIcon /> : <Bars3Icon />
+                }
+            </div>
+            {/* make list items inline for desktop with flex, hide menu for mobile with absolute position, md:static means position is static for desktop, perform animation with duration and ease-in */}
+            <ul className={`bg-blue-100 w-full h-20 text-start pl-12 md:flex justify-end items-center mr-24 absolute md:static duration-500 ease-in ${open ? 'top-20' : 'top-[-160px]'}`}>
+
+                <li className='text-blue-500 hover:text-teal-500 md:mr-8'>
+                    <Link to="/">Dashboard</Link>
+                </li>
+                <li className='text-blue-500 hover:text-teal-500 md:mr-8'>
+                    <Link to="/analytics">Analytics</Link>
+                </li>
+                <li className='text-blue-500 hover:text-teal-500'>
+                    <Link to="/blog">Blog</Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
